666f597e952bba406de168482a1194400613ad0a
Alexandre Calil Blasizza Paravani <acbp@users.noreply.github.com>
2014-10-17 15:31:07 -0300

    adicionada as Referências

--- Dijkstra/Dijkstra.js
+++ Dijkstra/Dijkstra.js
@@ -1,3 +1,4 @@
+/* Based on Artificial Intelligence for Games from Ian Millington (Author), John Funge (Author) */
 /**
  * Usando como base o _Grafo e seu conteudo.
  * Faz uma procura em todas as conexoes dos nos disponiveis procurando a menor ligacao
@@ -8,8 +9,8 @@
  */
 function Dijkstra(_grafo, _inicio, _fim) {
 
-    console.log('new dijkstra', arguments);
-    console.log('new dijkstra', _inicio,_fim);
+//    console.log('new dijkstra', arguments);
+//    console.log('new dijkstra', _inicio,_fim);
     var t = new Date();
 
     var aberta = new Caminho();
@@ -62,8 +63,7 @@ function Dijkstra(_grafo, _inicio, _fim) {
                     continue;
             }
             else {
-//                if( final.dado === {x:100,y:150})
-//                console.log('novo\n-', c,'\n-F',final,'\n-A',atual,'|\n');
+//                if( final.dado === {x:100,y:150}) console.log('novo\n-', c,'\n-F',final,'\n-A',atual,'|\n');
                 noFinal = new No();
                 noFinal.dado = final.dado;
                 noFinal.conexoes = [new Conexoes( atual, final, c.custo)];
